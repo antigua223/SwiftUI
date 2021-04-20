a7295787ba30d76708141ae2963f46200c788c16
LisetteAntigua <antigua223@gmail.com>
2021-04-20 16:08:03 -0400

    Initial Commit

---
+++ SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Lisette Antigua on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
